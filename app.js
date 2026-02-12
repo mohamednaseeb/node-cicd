db00cac5908abef1e9c3f9985b8c09505e610780
Mohamed Naseeb <mohamednaseeb@MacBook-Air.local>
2026-02-12 17:28:57 +0530

    fix container crash one

--- app.js
+++ app.js
@@ -6,13 +6,4 @@ app.get("/", (req,res)=>{
 
 app.listen(3000, "0.0.0.0", ()=>{
  console.log("Server running on port 3000");
-});const express = require("express");
-const app = express();
-
-app.get("/", (req,res)=>{
-    res.send("CI/CD Pipeline is Working 24/7🚀");
-});
-
-app.listen(3000, ()=>{
-    console.log("Server running on port 3000");
 });
