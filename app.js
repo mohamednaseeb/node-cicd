fb7ea9596811cec738ada4a507690a73d79ac98a
Mohamed Naseeb <mohamednaseeb@MacBook-Air.local>
2026-02-12 16:28:06 +0530

    CI/CD pipeline added

--- app.js
+++ app.js
@@ -2,6 +2,15 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req,res)=>{
+ res.send("CI/CD Pipeline is Working 🚀");
+});
+
+app.listen(3000, "0.0.0.0", ()=>{
+ console.log("Server running on port 3000");
+});const express = require("express");
+const app = express();
+
+app.get("/", (req,res)=>{
     res.send("CI/CD Pipeline is Working 🚀");
 });
 
